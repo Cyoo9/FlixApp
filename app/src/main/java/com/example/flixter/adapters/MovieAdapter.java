96f22d8846bc9365d44a41ba4285b1cf52af1cf2
Caleb Yoo <calebyoo33@gmail.com>
2021-02-25 20:49:52 -0800

    part 2 finished

--- app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixter/adapters/MovieAdapter.java
@@ -1,25 +1,35 @@
 package com.example.flixter.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.flixter.DetailActivity;
 import com.example.flixter.R;
 import com.example.flixter.models.Movie;
 
+import org.parceler.Parcels;
+
 import java.util.List;
 
-public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
 
     Context context;
     List<Movie> movies;
@@ -33,43 +43,18 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     //Usually involves inflating a layout from XML and returning the holder
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int position) {
         Log.d("MovieAdapter", "onCreateViewHolder");
-        RecyclerView.ViewHolder holder = null;
-        View movieView; 
-        switch(viewType) {
-            case 0: 
-                movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
-                holder = new ViewHolder(movieView);
-                break;
-            case 1:
-                movieView = LayoutInflater.from(context).inflate(R.layout.item_popular_movie, parent, false);
-                holder = new ViewHolder2(movieView); 
-                break;
-            default:
-                break;
-        }
-        return holder;
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Log.d("MovieAdapter", "onBindViewHolder " + position);
         //Get the movie at the passed position
-
         Movie movie = movies.get(position);
-        switch(holder.getItemViewType()) {
-            case 0:
-                ViewHolder movieView = (ViewHolder) holder;
-                movieView.bind(movie);
-                break;
-            case 1:
-                ViewHolder2 popularMovieView = (ViewHolder2)  holder;
-                popularMovieView.bindBackDropImage(movie);
-                break;
-            default:
-                break;
-        }
+        holder.bind(movie);
     }
 
 
@@ -77,18 +62,11 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     @Override
     public int getItemCount() {
         return movies.size();
-    }   
-
-    @Override
-    public int getItemViewType(int position) {
-        if(movies.get(position).getVoteAverage() >= 6) {
-            return 1; //1 is popular 
-        }
-        return 0; //0 is unpopular 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -98,6 +76,7 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
 
         }
 
@@ -115,28 +94,18 @@ public class MovieAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
             // then imageUrl = back drop image
             // else imageUrl = poster image
 
-            Glide.with(context).load(imageUrl).placeholder(R.drawable.hourglass).into(ivPoster);
+            //1. Register click listener on the whole row
+            //2. Navigate to a new activity on tap
+            Glide.with(context).load(imageUrl).placeholder(R.drawable.hourglass).transform(new RoundedCornersTransformation(30, 10)).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 
-    public class ViewHolder2 extends RecyclerView.ViewHolder {
-
-        ImageView ivPoster;
-
-        public ViewHolder2(@NonNull View itemView) {
-            super(itemView);
-            ivPoster = itemView.findViewById(R.id.ivPoster);
-
-        }
-
-        public void bindBackDropImage(Movie movie) {
-            String imageUrl;
-            // if phone is in landscape
-                imageUrl = movie.getBackdropPath();
-            // then imageUrl = back drop image
-            // else imageUrl = poster image
-
-            Glide.with(context).load(imageUrl).placeholder(R.drawable.hourglass).into(ivPoster);
-        }
-    }
 }
--- app/src/main/java/com/example/flixter/models/Movie.java
+++ app/src/main/java/com/example/flixter/models/Movie.java
@@ -3,16 +3,25 @@ package com.example.flixter.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
     double voteAverage;
+
+
+    public Movie() { //empty constructor for parceler library
+
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
@@ -20,6 +29,7 @@ public class Movie {
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         voteAverage = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -48,4 +58,6 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public int getMovieId() { return movieId; }
 }
